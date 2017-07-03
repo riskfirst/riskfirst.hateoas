b6758e7bd628ba4ac54a7c13cd8f80b575b22a90
jamie cohen <benson123@hotmail.com>
2017-07-03 11:49:56 +0100

    Changed assembly loading. Fixes #7

---
+++ Samples/RiskFirst.Hateoas.BasicSample/tests/RiskFirst.Hateos.BasicSample.Tests/LinksTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using Xunit;
+using RiskFirst.Hateoas.BasicSample;
+using Newtonsoft.Json;
+using RiskFirst.Hateoas.BasicSample.Models;
+using System.Collections.Generic;
+
+namespace RiskFirst.Hateos.BasicSample.Tests
+{
+    public class LinksTests
+    {
+        private readonly TestServer server;
+        private readonly HttpClient client;
+        public LinksTests()
+        {
+            // Arrange
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            client = server.CreateClient();
+        }
+        [Fact]
+        public async Task GetAllValues_ReturnsObjectsWithLinks()
+        {
+            // Act
+            var response = await client.GetAsync("/api/values");
+            response.EnsureSuccessStatusCode();
+            
+            var responseString = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ValuesContainer>(responseString);
+
+            Assert.All(values.Items, i => Assert.True(i.Links.Count>0,"Invalid number of links"));
+        }
+    }
+
+    public class ValuesContainer
+    {
+        public IEnumerable<ValueInfo> Items { get; set; }
+    }
+}
--- src/RiskFirst.Hateoas/DefaultRouteMap.cs
+++ src/RiskFirst.Hateoas/DefaultRouteMap.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -22,24 +23,34 @@ namespace RiskFirst.Hateoas
             this.contextAccessor = contextAccessor;
             this.logger = logger;
 
-            var asm = Assembly.GetEntryAssembly();
-           
-            var controllers = asm.GetTypes()
-                .Where(type => typeof(Controller).IsAssignableFrom(type));
+            var thisAssembly = typeof(DefaultRouteMap).GetTypeInfo().Assembly.GetName().Name;
+            var libraries =
+                DependencyContext.Default
+                                 .CompileLibraries
+                                 .Where(l => l.Dependencies.Any(d => d.Name.Equals(thisAssembly)));
 
-            var controllerMethods = controllers.SelectMany(c => c.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-                                                                 .Where(m => m.IsDefined(typeof(HttpMethodAttribute)))
-                                                                 .SelectMany(m => m.GetCustomAttributes<HttpMethodAttribute>(), (m, attr) => new
-                                                                 {
-                                                                     Controller = c,
-                                                                     Method = m,
-                                                                     HttpAttribute = attr
-                                                                 }));
-           
-            foreach (var attr in controllerMethods.Where(a => !String.IsNullOrWhiteSpace(a.HttpAttribute.Name)))
+            var names = libraries.Select(l => l.Name).Distinct();
+            var assemblies = names.Select(a => Assembly.Load(new AssemblyName(a)));
+
+            foreach (var asm in assemblies)
             {
-                var method = ParseMethod(attr.HttpAttribute.HttpMethods);
-                RouteMap[attr.HttpAttribute.Name] = new RouteInfo(attr.HttpAttribute.Name, method, new ReflectionControllerMethodInfo(attr.Method));
+                var controllers = asm.GetTypes()
+                    .Where(type => typeof(Controller).IsAssignableFrom(type));
+
+                var controllerMethods = controllers.SelectMany(c => c.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                                                                     .Where(m => m.IsDefined(typeof(HttpMethodAttribute)))
+                                                                     .SelectMany(m => m.GetCustomAttributes<HttpMethodAttribute>(), (m, attr) => new
+                                                                     {
+                                                                         Controller = c,
+                                                                         Method = m,
+                                                                         HttpAttribute = attr
+                                                                     }));
+
+                foreach (var attr in controllerMethods.Where(a => !String.IsNullOrWhiteSpace(a.HttpAttribute.Name)))
+                {
+                    var method = ParseMethod(attr.HttpAttribute.HttpMethods);
+                    RouteMap[attr.HttpAttribute.Name] = new RouteInfo(attr.HttpAttribute.Name, method, new ReflectionControllerMethodInfo(attr.Method));
+                }
             }
         }
                 
